a55760a88842022c6fd2efaa2de3bce71894c3af
hachimB <boubacarhachim@gmail.com>
2024-04-20 23:35:01 +0100

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,7 +2,7 @@
 """Module documentation"""
 
 
-def add(a : float, b : float) -> float:
+def add(a: float, b: float) -> float:
     """function add that takes a float a and a float
     b as arguments and returns their sum as a float."""
     return a + b
