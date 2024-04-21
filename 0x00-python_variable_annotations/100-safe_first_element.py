6a6a212096110d7f7d3514231e995722acf98151
hachimB <boubacarhachim@gmail.com>
2024-04-21 01:22:59 +0100

    100

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module documentation"""
+import typing
+
+
+def safe_first_element(lst: typing.Sequence[typing.Any]
+                       ) -> typing.Union[typing.Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
