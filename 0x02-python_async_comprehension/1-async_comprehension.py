d9f693a38c215ed7bd5b8e34e39599f4b420f660
hachimB <boubacarhachim@gmail.com>
2024-04-23 20:08:38 +0100

    async_generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Module documentation"""
-import asyncio
-import random
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> typing.List[float]:
     """async_comprehension"""
     return [i async for i in async_generator()]
