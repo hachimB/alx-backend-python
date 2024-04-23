3e65f474abe5b5a385b76fba4c0fd9367843b686
hachimB <boubacarhachim@gmail.com>
2024-04-23 01:29:48 +0100

    tasks2

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""Module documentation"""
+import asyncio
+import typing
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """wait_n async function"""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
