3ba000d7dc2e79c01584c390cd2fe565b6bd0b36
hachimB <boubacarhachim@gmail.com>
2024-04-23 16:26:07 +0100

    async_generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,6 +6,6 @@ import random
 
 async def async_generator():
     """ coroutine async_generator"""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 9)
+        yield random.uniform(0, 10)
