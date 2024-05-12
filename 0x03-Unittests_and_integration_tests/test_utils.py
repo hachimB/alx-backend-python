71f0a8525efcbccebc940c1a64873eae121fd407
hachimB <boubacarhachim@gmail.com>
2024-05-12 19:39:27 +0100

    memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -43,11 +43,12 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
 
+
 class TestGetJson(unittest.TestCase):
     """TestGetJson"""
     @parameterized.expand([
-         ("http://example.com", {"payload": True}),
-         ("http://holberton.io", {"payload": False}),
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
     ])
     @unittest.mock.patch('requests.get')
     def test_get_json(
@@ -60,24 +61,27 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(get_json(test_url), test_payload)
         mock_get.assert_called_once_with(test_url)
 
+
 class TestMemoize(unittest.TestCase):
     """TestMemoise"""
+
     def test_memoize(self):
         class TestClass:
             def a_method(self) -> int:
                 return 42
-        
+
             @memoize
             def a_property(self) -> Callable:
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+        with patch.object(TestClass,
+                          'a_method', return_value=42) as mock_method:
             test_obj = TestClass()
-            #First call
+            # First call
             result = test_obj.a_property
             self.assertEqual(result, 42)
             mock_method.assert_called_once()
-            #second call
+            # second call
             result2 = test_obj.a_property
             self.assertEqual(result2, 42)
             mock_method.assert_called_once()
